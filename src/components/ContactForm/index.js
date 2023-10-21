c0cd358d5b0cda89d4becea6e27d0241bf106da4
Hamza Khan <hamzasafdark@gmail.com>
2023-10-21 11:38:05 +0500

    input fields for contact and appointment forms added and styled using tailwind

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,1 +1,41 @@
 /* eslint-disable no-unused-vars */
+import React ,{useState} from "react";
+
+const ContactForm = () => {
+
+    const [inputName, setInputName] = useState("");
+    const [inputPhone, setInputPhone] = useState("");
+    const [inputEmail, setInputEmail] = useState("");
+
+    const handleNameChange = (e) => {
+        setInputName(e.target.value)
+    }
+
+    const handlePhoneChange = (e) => {
+        setInputName(e.target.value)
+    }
+
+    const handleEmailChange = (e) => {
+        setInputName(e.target.value)
+    }
+
+    return (
+        <div>
+            <form>
+            <label>Name</label><br/>
+            <input class='w-full h-8 outline-blue-500 outline-4' type="text" value={inputName} onChange={handleNameChange} required/>
+            <br />
+            <label>Phone</label><br/>
+            <input class='w-full h-8 outline-blue-500 outline-4' type="text" value={inputPhone} onChange={handlePhoneChange} required />
+            <br/>
+            <label>Email</label><br/>
+            <input class='w-full h-8 outline-blue-500 outline-4' type="text" value={inputEmail} onChange={handleEmailChange} required/>
+           
+            <button class='my-5 px-4 h-12 rounded-full bg-purple-500'>Add contact</button>
+           </form>
+        </div>
+        
+    )
+}
+
+export default ContactForm
