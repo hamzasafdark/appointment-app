e08142aa8f7043ffdee99d53448554e82587639f
Hamza Khan <hamzasafdark@gmail.com>
2023-10-21 11:43:36 +0500

    contact and appiointment form

--- src/components/AppointmentsPage/index.js
+++ src/components/AppointmentsPage/index.js
@@ -1,8 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import AppointmentForm from '../AppointmentForm'
 
 const AppointmentsPage = () => {
-
+    return (
+        <div class='px-5 bg-purple-500'>
+           <p class='font-bold text-xl'>Add Appointment</p>
+           <AppointmentForm />
+        </div>
+    )
 }
 
 export default AppointmentsPage
--- src/components/ContactsPage/index.js
+++ src/components/ContactsPage/index.js
@@ -1,37 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
+import ContactForm from '../ContactForm';
 
 const ContactsPage = () => {
-    const [inputName, setInputName] = useState("");
-    const [inputPhone, setInputPhone] = useState("");
-    const [inputEmail, setInputEmail] = useState("");
-
-    const handleNameChange = (e) => {
-        setInputName(e.target.value)
-    }
-
-    const handlePhoneChange = (e) => {
-        setInputName(e.target.value)
-    }
-
-    const handleEmailChange = (e) => {
-        setInputName(e.target.value)
-    }
-
-    return (
-        <div>
-           <p class='font-normal'>Add Contact</p>
-           <form>
-            <label>Name</label><br/>
-            <input type="text" value={inputName} onChange={handleNameChange} />
-            <br />
-            <label>Phone</label><br/>
-            <input type="text" value={inputPhone} onChange={handlePhoneChange} />
-            <br/>
-            <label>Email</label><br/>
-            <input type="text" value={inputEmail} onChange={handleEmailChange} />
-
-           </form>
+    return (        
+        <div class='px-5 bg-purple-500'>
+           <p class='font-bold text-xl'>Add Contact</p>
+           <ContactForm />
         </div>
     )
 }
