abde6702eb12865cb62736d218babf4d8139f5cb
Umair72Raza <umairraza95300@gmail.com>
2023-10-22 23:01:43 +0500

    recofigured props of Contactform uptil parent

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -2,52 +2,39 @@
 import React , {useEffect, useState} from "react";
 
 const ContactForm = (props) => {
-    console.log(props)
+    //console.log(props)
 
-    const [inputName, setInputName] = useState("");
-    const [inputPhone, setInputPhone] = useState("");
-    const [inputEmail, setInputEmail] = useState("");
+    const [contactData,setContactData] = useState(
+         {
+            name: "",
+            phone: "",
+            email: "",
+          }
+    );
 
-    const handleNameChange = (e) => {
-        setInputName(e.target.value)
+
+    const handleInputChange = (event) => {
+        const {name,value} = event.target;
+        setContactData({...contactData,[name]:value}) // it stores the particular input field's value in that variable using the name att of the input field
     }
 
-    const handlePhoneChange = (e) => {
-        setInputPhone(e.target.value)
+    const handleSubmit = (event) =>{
+        event.preventDefault(); //prevents reloading page on event 
+        props.postContact(contactData);  //props passed as a function with data in argument
     }
-
-    const handleEmailChange = (e) => {
-        setInputEmail(e.target.value)
-    }
-    
-    const handleSubmit = () => {
-        console.log(props.contactsList)
-    }
-
-    // useEffect((props) => {
-    //     const contact = {
-    //         name: inputName,
-    //         phone: inputPhone,
-    //         email: inputEmail
-    //     }
-    
-    //     props.updateContacts(contact)
-    // }, [inputName, inputEmail, inputPhone])
-
-
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
             <label class='text-white'>Name</label><br/>
-            <input class='w-full h-8 outline-blue-500 outline-4' type="text" value={inputName} onChange={handleNameChange} required/>
+            <input class='w-full h-8 outline-blue-500 outline-4 text-black' name="name" type="text" value={contactData.name} onChange={handleInputChange} required/>
             <br />
             <label  class='text-white'>Phone</label><br/>
-            <input class='w-full h-8 outline-blue-500 outline-4' type="text" value={inputPhone} onChange={handlePhoneChange} required />
+            <input class='w-full h-8 outline-blue-500 outline-4 text-black' name="phone" type="text" value={contactData.phone} onChange={handleInputChange} required />
             <br/>
             <label  class='text-white'>Email</label><br/>
-            <input class='w-full h-8 outline-blue-500 outline-4' type="text" value={inputEmail} onChange={handleEmailChange} required/>
+            <input class='w-full h-8 outline-blue-500 outline-4 text-black' name="email" type="text" value={contactData.email} onChange={handleInputChange} required/>
         
-            <button class='my-5 px-4 h-12 rounded-full text-white bg-purple-900 hover:bg-purple-950' onSubmit={handleSubmit}>Add contact</button>
+            <button class='my-5 px-4 h-12 rounded-full text-white bg-purple-900 hover:bg-purple-950' type="Submit">Add contact</button>
            </form>
         </div>
     )
